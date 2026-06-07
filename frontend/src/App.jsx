2e3ca65a20e8ddf6684da780358329bea5fffb7e
arnabdas9039 <arnabdas.9039@gmail.com>
2026-06-07 10:50:17 +0530

    A whole lot changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import DemoHome from "./pages/DemoHome";
 import { ExperienceDetails } from "./pages/ExperienceDetails";
 import MyBookings from "./pages/MyBookings";
-import Navbar from "./components/NewNavbar";
+import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 import { ModalProvider } from "./context/ModalContext";
@@ -43,7 +43,7 @@ function AppContent() {
       <Navbar />
       {isLoginModalOpen && <LoginSignup />}
       <Chatbot />
-      <div className="">
+      <div className="pt-[73px]">
         <Routes>
           <Route path="/" element={<DemoHome />} />
           <Route path="/old-home" element={<Home />} />
